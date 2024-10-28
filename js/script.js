f48ca2323c347b604f272399f0b2c59d521a3c3d
yana-hris <yana.hristova.work@gmail.com>
2024-10-28 16:22:10 +0200

    Initial commit of portfolio site

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  new Typed("#typing-text", {
+    strings: ["Love Turning Ideas into Reality with Code."],
+    typeSpeed: 50,
+    backSpeed: 30,
+    loop: true,
+    showCursor: false,
+  });
+});
